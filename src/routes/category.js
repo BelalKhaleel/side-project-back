13ea848f1f4cc2d7e0d551a238a805acb46db04a
Belal Khaleel <belalkhaleel19@gmail.com>
2023-04-17 22:22:16 +0300

    added image upload middleware

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import controller from "../controllers/category.js";
+import checkAuth from "../middleware/check-auth.js";
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
-router.post("/", controller.post);
-router.put("/:id", controller.put);
-router.delete("/:id", controller.delete);
+router.post("/",checkAuth, controller.post);
+router.put("/:id",checkAuth, controller.put);
+router.delete("/:id",checkAuth, controller.delete);
 
 export default router;
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,15 @@
 import express from "express";
 const router = express.Router();
 import controller from "../controllers/product.js";
+import checkAuth from "../middleware/check-auth.js";
+import upload from "../middleware/image-upload.js";
 
 router.get("/all", controller.getAll);
 router.get("/", controller.get);
 router.get("/:id", controller.getById);
-router.post("/", controller.post);
-router.put("/:id", controller.put);
-router.patch("/", controller.softDelete);
-router.delete("/:id", controller.delete);
+router.post("/",checkAuth, upload.single("image"), controller.post);
+router.put("/:id",checkAuth, controller.put);
+router.patch("/",checkAuth, controller.softDelete);
+router.delete("/:id",checkAuth, controller.delete);
 
 export default router;
